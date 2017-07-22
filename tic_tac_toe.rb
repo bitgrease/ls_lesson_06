506af2a0ef106a2b4c8cb6db21af695b3f713699
Kelly Netterville <bitgrease@gmail.com>
2017-07-22 14:22:05 -0400

    Added tournament scoring to 5. Also fixed WINNING_POSITIONS array. Final element was not correct.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -5,7 +5,7 @@ ENOUGH_MOVES_FOR_WIN = 5
 TIE = false
 WINNING_POSITIONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
                      [1, 4, 7], [2, 5, 8], [3, 6, 9],
-                     [1, 5, 9], [3, 6, 9]]
+                     [1, 5, 9], [3, 5, 7]]
 
 def prompt_user(str)
   puts "=> #{str}"
@@ -23,10 +23,11 @@ def joinor(squares, separator=',', word='or')
 end
 
 # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-def display_board(board)
+def display_board(board, player_wins, computer_wins)
   system 'clear'
   system 'cls'
   puts "You are #{PLAYER_PIECE} and Computer is #{COMPUTER_PIECE}."
+  puts "Tournament Score: Player #{player_wins} - Computer #{computer_wins}"
   puts ''
   puts '     |     |'
   puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}"
@@ -91,27 +92,55 @@ def calculate_winner(board)
   TIE
 end
 
+game_counter = computer_wins = player_wins = 0
+
 loop do
   board = initialize_board
 
+
   loop do
-    display_board(board)
+    display_board(board, player_wins, computer_wins)
     player_places_piece!(board)
     if number_squares_used(board) >= ENOUGH_MOVES_FOR_WIN
       break if someone_won?(board) || board_full?(board)
     end
-    display_board(board)
+    display_board(board, player_wins, computer_wins)
 
     puts 'Computer choses a square'
     sleep 1
-    display_board(board)
+    display_board(board, player_wins, computer_wins)
     computer_places_piece!(board)
     if number_squares_used(board) >= ENOUGH_MOVES_FOR_WIN
       break if someone_won?(board)
     end
   end
 
-  display_board(board)
-  prompt_user "#{calculate_winner(board) || 'No one'} won. Play again? (y/n)"
-  break unless gets.chomp.downcase.chr.eql? 'y'
+  display_board(board, player_wins, computer_wins)
+
+  winner = calculate_winner(board)
+  case winner
+  when 'Player'
+    player_wins += 1
+    game_counter += 1
+  when 'Computer'
+    computer_wins += 1
+    game_counter += 1
+  end
+
+  prompt_user "#{winner || 'No one'} won."
+  prompt_user "Player #{player_wins} - Computer #{computer_wins}"
+  display_board(board, player_wins, computer_wins) # to update top of screen
+
+  if game_counter < 5 || (computer_wins < 5 && player_wins < 5)
+      prompt_user 'Play again? (y/n)'  
+      break unless gets.chomp.downcase.chr.eql? 'y'
+  elsif player_wins >=5 || computer_wins >= 5
+    if player_wins >= 5
+      prompt_user 'You won the tournament!. Play another tourney (y/n)?'
+    elsif computer_wins >= 5
+      prompt_user 'Computer won the tournament. Play another tourney (y/n)?'
+    end
+    break unless gets.chomp.downcase.chr.eql? 'y'
+    game_counter = computer_wins = player_wins = 0
+  end
 end
