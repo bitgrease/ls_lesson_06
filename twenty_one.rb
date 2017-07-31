7a2eddf30c0b159d867701369c5ff60ed6327ebe
Kelly Netterville <bitgrease@gmail.com>
2017-07-31 17:34:23 -0400

    Final changes for now. Clean rubocop and reviewed solution.

--- twenty_one.rb
+++ twenty_one.rb
@@ -1,8 +1,16 @@
 SUITS_AND_FACES = { d: 'Diamonds', s: 'Spades', c: 'Clubs', h: 'Hearts',
                     a: 'Ace', k: 'King', q: 'Queen', j: 'Jack' }
+TWENTY_ONE = 21
+DEALER_STAY = 17
+FACE_VALUE = 10
+HIGH_ACE = 11
 
 def clear_screen
   system('clear') || system('cls')
+end
+
+def prompt(msg)
+  puts "=> #{msg}"
 end
 
 def user_y_or_n
@@ -10,7 +18,7 @@ def user_y_or_n
   loop do
     entered_choice = gets.chomp.downcase.chr
     return entered_choice if %w[y n].include? entered_choice
-    puts 'Invalid Choice. Choose "y" or "n".'
+    prompt 'Invalid Choice. Choose "y" or "n".'
   end
 end
 
@@ -43,71 +51,73 @@ def create_shuffled_deck
   deck.shuffle
 end
 
-def deal_cards(player_hand, deck, initial_deal=false)
-  initial_deal ? 2.times { player_hand << deck.pop } : player_hand << deck.pop
-end
-
-def show_hand(player_hand, is_dealer=false, reveal=false)
-  if is_dealer
-    if reveal
-      puts "Dealer has #{joinand(player_hand)}."
-    else
-      puts "Dealer has #{card_name(player_hand[0])} showing."
-    end
+def deal_cards(card_hand, deck, deal_type=:regular)
+  if deal_type.eql? :initial
+    2.times { card_hand << deck.pop }
   else
-    puts "You have #{joinand(player_hand)}."
+    card_hand << deck.pop
   end
 end
 
-def player_win_or_bust?(player_hand)
-  hand_value(player_hand) == 21 || busted?(player_hand)
+def show_hand(card_hand, player=:player, cards_up=:one)
+  if player.eql? :dealer
+    if cards_up.eql? :two
+      prompt "Dealer has #{joinand(card_hand)}."
+    else
+      prompt "Dealer has #{card_name(card_hand[0])} showing."
+    end
+  else
+    prompt "You have #{joinand(card_hand)}."
+  end
+end
+
+def player_win_or_bust?(card_total)
+  card_total == TWENTY_ONE || busted?(card_total)
 end
 
 def hand_value(hand)
   total = 0
   values = hand.map { |card| card[1] }
   values.each do |value|
-    if %w[1 2 3 4 5 6 7 8 9 10].include? value
-      total += value.to_i
-    elsif %w[k q j].include? value
-      total += 10
-    else
-      total += 11
-    end
+    total += if %w[1 2 3 4 5 6 7 8 9 10].include? value
+               value.to_i
+             elsif %w[k q j].include? value
+               FACE_VALUE
+             else
+               HIGH_ACE
+             end
   end
 
   values.select { |value| value.eql? 'a' }.count.times do
-    total -= 10 if total > 21
+    total -= FACE_VALUE if total > TWENTY_ONE
   end
   total
 end
 
-def busted?(card_hand)
-  hand_value(card_hand) > 21
+def busted?(card_total)
+  card_total > TWENTY_ONE
 end
 
 def hit_or_stay
   loop do
-    puts 'Hit or Stay? (h or s)'
+    prompt 'Hit or Stay? (h or s)'
     choice = gets.chomp.downcase.chr
     return choice if %w[h s].include? choice
-    puts 'Invalid choice!'
+    prompt 'Invalid choice!'
   end
 end
 
 def player_turn(player_hand, dealer_hand, deck)
   loop do
     choice = hit_or_stay
-    if choice.eql? 's'
-      puts 'You chose to stay.'
-      break
-    end
+    prompt 'You chose to stay.' && break if choice.eql? 's'
     player_hand << deck.pop
+    card_total = hand_value(player_hand)
     clear_screen
     show_both_hands(player_hand, dealer_hand)
-    if player_win_or_bust?(player_hand)
-      puts hand_value(player_hand) == 21 ? 'You got 21!' : 'You Bust!'
-      sleep(1)
+    if player_win_or_bust?(card_total)
+      prompt card_total == TWENTY_ONE ? 'You got 21!' : 'You Bust!'
+      sleep 1
       break
     end
   end
@@ -115,34 +125,41 @@ end
 
 def dealer_turn(dealer_hand, deck)
   loop do
-    if hand_value(dealer_hand) >= 17
-      puts busted?(dealer_hand) ? 'Dealer Busted!' : 'Dealer Stays.'
+    card_total = hand_value(dealer_hand)
+    if card_total >= DEALER_STAY
+      prompt busted?(card_total) ? 'Dealer Busted!' : 'Dealer Stays.'
       sleep 1
       break
     end
-    puts 'Dealer hits...'
+    prompt 'Dealer hits...'
     sleep 1
     dealer_hand << deck.pop
-    show_hand(dealer_hand, true, true)
+    show_hand(dealer_hand, :dealer, :two)
   end
 end
 
 def find_winner(player_hand, dealer_hand)
-  return 'Player' if hand_value(player_hand) == 21 || busted?(dealer_hand)
-  return 'Dealer' if busted?(player_hand) ||
-                     hand_value(dealer_hand) > hand_value(player_hand)
+  player_total = hand_value(player_hand)
+  dealer_total = hand_value(dealer_hand)
+  if player_total == TWENTY_ONE || busted?(dealer_total)
+    return 'Player'
+  elsif busted?(player_total) || dealer_total > player_total
+    return 'Dealer'
+  elsif player_total == dealer_total
+    return 'Tie. No one'
+  end
   'Player'
 end
 
 def show_winner(player_hand, dealer_hand)
-  puts "Player has #{hand_value(player_hand)}."
-  puts "Dealer has #{hand_value(dealer_hand)}."
-  puts "#{find_winner(player_hand, dealer_hand)} wins."
+  prompt "Player has #{hand_value(player_hand)}."
+  prompt "Dealer has #{hand_value(dealer_hand)}."
+  prompt "#{find_winner(player_hand, dealer_hand)} wins."
 end
 
-def show_both_hands(player_hand, dealer_hand, reveal=false)
+def show_both_hands(player_hand, dealer_hand, cards_up=:one)
   show_hand(player_hand)
-  show_hand(dealer_hand, true, reveal)
+  show_hand(dealer_hand, :dealer, cards_up)
 end
 
 loop do
@@ -151,22 +168,24 @@ loop do
   dealer_hand = []
   deck = create_shuffled_deck
 
-  deal_cards(player_hand, deck, true)
-  deal_cards(dealer_hand, deck, true)
+  deal_cards(player_hand, deck, :initial)
+  deal_cards(dealer_hand, deck, :initial)
 
-  if hand_value(player_hand) != 21
+  if hand_value(player_hand) != TWENTY_ONE
     show_both_hands(player_hand, dealer_hand)
     player_turn(player_hand, dealer_hand, deck)
     clear_screen
-    show_both_hands(player_hand, dealer_hand, true)
-    dealer_turn(dealer_hand, deck) unless player_win_or_bust?(player_hand)
+    show_both_hands(player_hand, dealer_hand, :two)
+    unless player_win_or_bust?(hand_value(player_hand))
+      dealer_turn(dealer_hand, deck)
+    end
   else
     clear_screen
-    show_both_hands(player_hand, dealer_hand, true)
-    puts 'You got 21 on the deal!'
+    show_both_hands(player_hand, dealer_hand, :two)
+    prompt 'You got 21 on the deal!'
   end
 
   show_winner(player_hand, dealer_hand)
-  puts 'Do you want to play again? (y/n)'
+  prompt 'Do you want to play again? (y/n)'
   break if user_y_or_n.eql? 'n'
 end
